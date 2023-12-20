ce04d56f16dc7779eccc6e56752df4b05e26f38b
PlotRetro <dhumpirz@gmail.com>
2023-12-20 16:35:28 -0300

    Update: Páginas adicionar e editar receitas
    Referente ao componente de adicionar foto e vídeo. Ainda não está concluído. Falta responsividade;
    Além disso foi atualizado o Javascript da página de recuperar senha.

--- src/js/passwordToggle.js
+++ src/js/passwordToggle.js
@@ -1,5 +1,4 @@
-//Função de alternar o texto entre visivel e não visivel
-
+// Função para mostrar a senha
 function mostrarSenha(inputId, imgElement) {
   var inputPass = document.getElementById(inputId);
   var siblingImgElement =
@@ -15,3 +14,83 @@ function mostrarSenha(inputId, imgElement) {
     siblingImgElement.style.display = "block";
   }
 }
+
+// Função para validar a senha
+function validarSenha(senha) {
+  var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return regex.test(senha);
+}
+
+// Função para validar se o usuário digitou a senha corretamente antes do envio
+document.querySelector('.row-item').addEventListener('submit', function(event) {
+  var newPassword = document.getElementById('newPassword');
+  var confirmPassword = document.getElementById('confirmPassword');
+
+  if (!newPassword.value || !confirmPassword.value) {
+    alert('Por favor, preencha todos os campos antes de salvar.');
+    event.preventDefault();
+  } else if (!validarSenha(newPassword.value)) {
+    alert('A senha deve ter no mínimo 8 caracteres, 1 símbolo, 1 número e 1 letra maiúscula.');
+    event.preventDefault();
+  } else if (newPassword.value !== confirmPassword.value) {
+    alert('As duas senhas não correspondem.');
+    event.preventDefault();
+  }
+});
+
+// Função para mostrar os requisitos da senha
+document.getElementById('newPassword').addEventListener('focus', function() {
+  var infoBalloon = document.createElement('div');
+  infoBalloon.innerHTML = '<ul id="password-requirements" style="list-style-type: none;">' +
+    '<li id="length">Pelo menos 8 caracteres</li>' +
+    '<li id="lowercase">Pelo menos 1 letra minúscula</li>' +
+    '<li id="uppercase">Pelo menos 1 letra maiúscula</li>' +
+    '<li id="number">Pelo menos 1 número</li>' +
+    '<li id="symbol">Pelo menos 1 símbolo</li>' +
+    '</ul>';
+  infoBalloon.style.position = 'absolute';
+  infoBalloon.style.backgroundColor = '#333';
+  infoBalloon.style.color = 'white';
+  infoBalloon.style.padding = '10px';
+  infoBalloon.style.borderRadius = '5px';
+  infoBalloon.style.bottom = '70px';
+  infoBalloon.style.right = '20px';
+  this.parentNode.appendChild(infoBalloon);
+});
+
+//Para validar indicando se tal requisito foi cumprido ou não no balão de requisitos
+document.getElementById('newPassword').addEventListener('input', function() {
+  var requirements = document.getElementById('password-requirements');
+  if (this.value.length >= 8) {
+    requirements.querySelector('#length').style.color = 'green';
+  } else {
+    requirements.querySelector('#length').style.color = 'red';
+  }
+  if (/[a-z]/.test(this.value)) {
+    requirements.querySelector('#lowercase').style.color = 'green';
+  } else {
+    requirements.querySelector('#lowercase').style.color = 'red';
+  }
+  if (/[A-Z]/.test(this.value)) {
+    requirements.querySelector('#uppercase').style.color = 'green';
+  } else {
+    requirements.querySelector('#uppercase').style.color = 'red';
+  }
+  if (/\d/.test(this.value)) {
+    requirements.querySelector('#number').style.color = 'green';
+  } else {
+    requirements.querySelector('#number').style.color = 'red';
+  }
+  if (/[@$!%*?&]/.test(this.value)) {
+    requirements.querySelector('#symbol').style.color = 'green';
+  } else {
+    requirements.querySelector('#symbol').style.color = 'red';
+  }
+});
+
+document.getElementById('newPassword').addEventListener('blur', function() {
+  var infoBalloon = this.parentNode.querySelector('div');
+  if (infoBalloon) {
+    this.parentNode.removeChild(infoBalloon);
+  }
+});
